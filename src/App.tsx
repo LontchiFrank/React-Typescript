a1e005d802cff96aa9d2e5b8b5fb8f5ee8c2c05f
LontchiFrank <fodjof6@gmail.com>
2023-05-15 17:20:09 +0100

    added User

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { PersonList } from "./components/PersonLists";
 import { Status } from "./components/Status";
 import { Heads } from "./components/Heads";
 import { Oscar } from "./components/Oscar";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
 
 function App() {
   const personName = {
@@ -30,11 +32,10 @@ function App() {
 
   return (
     <div className="App">
-      <Status status="success" />
-      <Oscar>
-        <Heads>Oscar goes to Leonardo dicarprio </Heads>
-      </Oscar>
-      <Greet name="Franky" isLoggedIn={false} />
+      <Button
+        handleClick={(event, id) => console.log("Button Clicked", event, id)}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
